47d7528b26d1f46f0abaa1de994059d0b84d8efa
LeonardHolter <leonardholter17@outlook.com>
2024-02-20 11:39:02 +0100

    Initial Commit

---
+++ rock-paper-scissors/rock_paper_scissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  rock_paper_scissorsApp.swift
+//  rock-paper-scissors
+//
+//  Created by Leonard Holter on 20/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct rock_paper_scissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
